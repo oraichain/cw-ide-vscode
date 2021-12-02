5107d689adcb115a8b79139b21c81a194db52b79
piggy2303 <nguyentuananh23031997@gmail.com>
2021-12-02 07:08:19 +0000

    update style text

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { useEffect, useState } from 'react';
-import Wasm from './lib/wasm';
-import logo from './logo.png';
+import { useEffect, useState } from "react";
+import Wasm from "./lib/wasm";
+import logo from "./logo.png";
 // import Form from "@rjsf/core";
-import Form from '@rjsf/antd';
-import './themes/style.scss';
-import { Input, Select, Spin } from 'antd';
-import { ReactComponent as IconSelect } from './assets/icons/code.svg';
-import { ReactComponent as IconChain } from './assets/icons/chain.svg';
-import { LoadingOutlined } from '@ant-design/icons';
+import Form from "@rjsf/antd";
+import "./themes/style.scss";
+import { Input, Select, Spin } from "antd";
+import { ReactComponent as IconSelect } from "./assets/icons/code.svg";
+import { ReactComponent as IconChain } from "./assets/icons/chain.svg";
+import { LoadingOutlined } from "@ant-design/icons";
 
-const antIcon = <LoadingOutlined style={{ fontSize: 24, color: "#7954FF" }} spin />;
+const antIcon = (
+  <LoadingOutlined style={{ fontSize: 24, color: "#7954FF" }} spin />
+);
 
 const { Option } = Select;
 
@@ -18,13 +20,16 @@ let vscode: VSCode;
 const App = () => {
   const DEFAULT_CHAINMAME = window.chainStore.chainInfos[0].chainName;
 
+  const [isBuilt, setIsBuilt] = useState(false);
+  const [isDeployed, setIsDeployed] = useState(false);
+
   const [action, setAction] = useState();
   const [wasmBody, setWasmBody] = useState();
-  const [label, setLabel] = useState('');
+  const [label, setLabel] = useState("");
   const [chainName, setChainName] = useState(DEFAULT_CHAINMAME);
-  const [initInput, setInitInput] = useState('');
-  const [contractAddr, setContractAddr] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [initInput, setInitInput] = useState("");
+  const [contractAddr, setContractAddr] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [initSchema, setInitSchema] = useState({});
   const [querySchema, setQuerySchema] = useState({});
   const [handleSchema, setHandleSchema] = useState({});
@@ -52,38 +57,55 @@ const App = () => {
       setInitSchema(JSON.parse(message.schemaFile));
       setHandleSchema({});
       setQuerySchema({});
+      setIsBuilt(true);
+      setErrorMessage("");
     }
     if (message.action === "deploy") {
       setInitSchema({});
       setHandleSchema(JSON.parse(message.handleFile));
       setQuerySchema(JSON.parse(message.queryFile));
+      setIsBuilt(false);
     }
   };
   useEffect(() => {
-    window.addEventListener('message', eventHandler);
+    window.addEventListener("message", eventHandler);
     return () => {
-      window.removeEventListener('message', eventHandler);
+      window.removeEventListener("message", eventHandler);
     };
   });
 
   const onDeploy = async (mnemonic: any, wasmBytes?: any) => {
-    setErrorMessage('');
+    setErrorMessage("");
     console.log("mnemonic in on deploy: ", mnemonic);
     console.log("chain name: ", chainName);
     window.chainStore.setChain(chainName);
     setIsLoading(true);
-    setContractAddr('');
+    setContractAddr("");
 
     try {
-      let address = await Wasm.handleDeploy({ mnemonic, wasmBody: wasmBytes ? wasmBytes : wasmBody, initInput, label, sourceCode: '' });
+      let address = await Wasm.handleDeploy({
+        mnemonic,
+        wasmBody: wasmBytes ? wasmBytes : wasmBody,
+        initInput,
+        label,
+        sourceCode: "",
+      });
       console.log("contract address: ", address);
       setContractAddr(address);
+      setIsDeployed(true);
       setIsLoading(false);
     } catch (error) {
+      setIsDeployed(false);
       setIsLoading(false);
       setErrorMessage(JSON.stringify(error));
     }
-  }
+  };
+
+  const handleMinter = (minter) => {
+    setInitSchema(minter);
+  };
+
+  console.log(initSchema);
 
   return (
     <div className="app">
@@ -94,57 +116,84 @@ const App = () => {
       <div className="app-divider" />
       <div className="app-body">
         <div className="chain-select">
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <IconChain style={{ width: '16px', height: '16px', marginRight: '5px', marginBottom: '8px' }} />
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <IconChain
+              style={{
+                width: "16px",
+                height: "16px",
+                marginRight: "5px",
+                marginBottom: "8px",
+              }}
+            />
             <h3> Select chain name</h3>
           </div>
-          <Select defaultValue={DEFAULT_CHAINMAME} style={{ width: 240 }} suffixIcon={<IconSelect />} onSelect={value => {
-            setChainName(value);
-            window.chainStore.setChain(value);
-          }}>
-            {
-              window.chainStore.chainInfos.map(info =>
-                <Option key={info.chainName} value={info.chainName}>{info.chainName}</Option>
-              )}
+          <Select
+            defaultValue={DEFAULT_CHAINMAME}
+            style={{ width: 240 }}
+            suffixIcon={<IconSelect />}
+            onSelect={(value) => {
+              setChainName(value);
+              window.chainStore.setChain(value);
+            }}
+          >
+            {window.chainStore.chainInfos.map((info) => (
+              <Option key={info.chainName} value={info.chainName}>
+                {info.chainName}
+              </Option>
+            ))}
           </Select>
         </div>
         <div className="wrap-form">
           <span className="please-text">Please fill out the form below:</span>
-          <div className="input-form">
+          {/* <div className="input-form">
             <h4>init contract</h4>
             <Input placeholder={`eg. {"minter": "ADDRESS"}`} value={initInput}
               onInput={(e: any) => setInitInput(e.target.value)} />
-          </div>
+          </div> */}
           <div className="input-form">
             <h4>input label</h4>
-            <Input placeholder="eg. random text" value={label}
-              onInput={(e: any) => setLabel(e.target.value)} />
+            <Input
+              placeholder="eg. random text"
+              value={label}
+              onInput={(e: any) => setLabel(e.target.value)}
+            />
           </div>
         </div>
       </div>
 
-      {!isLoading ?
-        ((contractAddr &&
+      {!isLoading ? (
+        (contractAddr && (
           <div className="contract-address">
             <span>Contract address </span>
             <p>{contractAddr}</p>
-          </div>)
-          ||
-          (errorMessage &&
-            <div className="contract-address">
-              <span style={{ color: "red" }}>Error message </span>
-              <p>{errorMessage}</p>
-            </div>))
-        :
+          </div>
+        )) ||
+        (errorMessage && (
+          <div className="contract-address">
+            <span style={{ color: "red" }}>Error message </span>
+            <p>{errorMessage}</p>
+          </div>
+        ))
+      ) : (
         <div className="deploying">
           <Spin indicator={antIcon} />
           <span>Deploying ...</span>
-        </div>}
-      {initSchema ? (<Form schema={initSchema} onSubmit={(data) => { console.log("ABCD: ", data) }} />) : ''}
-      {handleSchema ? <Form schema={handleSchema} /> : ''}
-      {querySchema ? <Form schema={querySchema} /> : ''}
-    </div >
+        </div>
+      )}
 
+      {isBuilt && (
+        <Form
+          schema={initSchema}
+          onSubmit={(data) => handleMinter(data?.formData)}
+        />
+      )}
+      {isDeployed && (
+        <>
+          <Form schema={handleSchema} />
+          <Form schema={querySchema} />
+        </>
+      )}
+    </div>
   );
 };
 
