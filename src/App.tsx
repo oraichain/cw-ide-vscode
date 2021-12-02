e7536daaa27391c3c913daa8cd599b2c0a086410
piggy2303 <nguyentuananh23031997@gmail.com>
2021-12-02 01:19:13 +0000

    update icon cosmoswasm

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react';
 import Wasm from './lib/wasm';
 import logo from './logo.png';
-import Form from "@rjsf/core";
+// import Form from "@rjsf/core";
+import Form from '@rjsf/antd';
 import './themes/style.scss';
 import { Input, Select, Spin } from 'antd';
 import { ReactComponent as IconSelect } from './assets/icons/code.svg';
@@ -139,7 +140,7 @@ const App = () => {
           <Spin indicator={antIcon} />
           <span>Deploying ...</span>
         </div>}
-      {initSchema ? <Form schema={initSchema} /> : ''}
+      {initSchema ? (<Form schema={initSchema} onSubmit={(data) => { console.log("ABCD: ", data) }} />) : ''}
       {handleSchema ? <Form schema={handleSchema} /> : ''}
       {querySchema ? <Form schema={querySchema} /> : ''}
     </div >
