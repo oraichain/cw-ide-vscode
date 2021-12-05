60c0d5f53c1cf87cd396795022b1956b9dfb9601
ducphamle2 <ducphamle212@gmail.com>
2021-12-05 20:56:32 +0700

    update state check build & deploy and add intro screen

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import { ReactComponent as IconSelect } from './assets/icons/code.svg';
 import { ReactComponent as IconChain } from './assets/icons/chain.svg';
 import { LoadingOutlined } from '@ant-design/icons';
 import CosmJs from './lib/cosmjs';
+import _ from "lodash";
 
 const antIcon = (
   <LoadingOutlined style={{ fontSize: 24, color: "#7954FF" }} spin />
@@ -21,30 +22,24 @@ let vscode: VSCode;
 const App = () => {
   const DEFAULT_CHAINMAME = window.chainStore.chainInfos[0].chainName;
 
+  const [initSchemaData, setInitSchemaData] = useState({});
   const [isBuilt, setIsBuilt] = useState(false);
   const [isDeployed, setIsDeployed] = useState(false);
-
-  const [action, setAction] = useState();
   const [wasmBody, setWasmBody] = useState();
   const [label, setLabel] = useState('');
   const [gasPrice, setGasPrice] = useState('0');
   const [gasDenom, setGasDenom] = useState(window.chainStore.chainInfos[0].feeCurrencies[0].coinMinimalDenom);
   const [chainName, setChainName] = useState(DEFAULT_CHAINMAME);
-  const [initInput, setInitInput] = useState("");
   const [contractAddr, setContractAddr] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [initSchema, setInitSchema] = useState({});
   const [querySchema, setQuerySchema] = useState({});
   const [handleSchema, setHandleSchema] = useState({});
-  const [initSchemaData, setInitSchemaData] = useState({});
-
+  // const [initSchemaData, setInitSchemaData] = useState({});
   const [isLoading, setIsLoading] = useState(false);
   // Handle messages sent from the extension to the webview
   const eventHandler = (event: MessageEvent) => {
     const message = event.data; // The json data that the extension sent
-    // console.log('event: ', event);
-    // console.log('message: ', message);
-    setAction(message.action);
     if (message.payload) setWasmBody(message.payload);
     if (message.mnemonic) {
       onDeploy(message.mnemonic, message.payload);
@@ -62,13 +57,12 @@ const App = () => {
       setHandleSchema({});
       setQuerySchema({});
       setIsBuilt(true);
+      setIsDeployed(false);
       setErrorMessage("");
     }
     if (message.action === "deploy") {
-      setInitSchema({});
       setHandleSchema(JSON.parse(message.handleFile));
       setQuerySchema(JSON.parse(message.queryFile));
-      setIsBuilt(false);
     }
   };
   useEffect(() => {
@@ -78,12 +72,18 @@ const App = () => {
     };
   });
 
+  const handleOnChange = ({ formData }, e) => {
+    // setInitSchemaData(formData);
+    console.log("Data submitted: ", formData)
+  };
+
   const onDeploy = async (mnemonic: any, wasmBytes?: any) => {
-    if (!initSchemaData) return;
-    console.log("init schema: ", initSchemaData);
+    console.log("Instantiate data: ", initSchemaData);
+    if (_.isEmpty(initSchemaData)) {
+      setErrorMessage("Instantiate data is empty!");
+      return;
+    };
     setErrorMessage("");
-    console.log("mnemonic in on deploy: ", mnemonic);
-    console.log("chain name: ", chainName);
     window.chainStore.setChain(chainName);
     setIsLoading(true);
     setContractAddr("");
@@ -94,16 +94,15 @@ const App = () => {
       console.log("contract address: ", address);
       setContractAddr(address);
       setIsDeployed(true);
+      setIsBuilt(false);
+      setInitSchema({});
       setIsLoading(false);
     } catch (error) {
       setIsDeployed(false);
       setIsLoading(false);
-      setErrorMessage(JSON.stringify(error));
+      setIsBuilt(true);
+      setErrorMessage(String(error));
     }
-  };
-
-  const handleMinter = (minter) => {
-    setInitSchemaData(minter);
   };
 
   const onQuery = async (data) => {
@@ -112,8 +111,6 @@ const App = () => {
     console.log("query result: ", queryResult);
   }
 
-  console.log(initSchema);
-
   return (
     <div className="app">
       <header className="app-header">
@@ -121,64 +118,69 @@ const App = () => {
         <h1 className="app-title">COSMWASM IDE</h1>
       </header>
       <div className="app-divider" />
-      <div className="app-body">
-        <div className="chain-select">
-          <div style={{ display: "flex", alignItems: "center" }}>
-            <IconChain
-              style={{
-                width: "16px",
-                height: "16px",
-                marginRight: "5px",
-                marginBottom: "8px",
-              }}
+      {
+        isBuilt && (
+          <div>
+            <div className="app-body">
+              <div className="chain-select">
+                <div style={{ display: "flex", alignItems: "center" }}>
+                  <IconChain
+                    style={{
+                      width: "16px",
+                      height: "16px",
+                      marginRight: "5px",
+                      marginBottom: "8px",
+                    }}
+                  />
+                  <h3> Select chain name</h3>
+                </div>
+                <Select
+                  defaultValue={DEFAULT_CHAINMAME}
+                  style={{ width: 240 }}
+                  suffixIcon={<IconSelect />}
+                  onSelect={(value) => {
+                    setChainName(value);
+                    window.chainStore.setChain(value);
+                    setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
+                  }}
+                >
+                  {window.chainStore.chainInfos.map((info) => (
+                    <Option key={info.chainName} value={info.chainName}>
+                      {info.chainName}
+                    </Option>
+                  ))}
+                </Select>
+              </div>
+              <div className="wrap-form">
+                <span className="please-text">Please fill out the form below to deploy the contract:</span>
+                <div className="input-form">
+                  <h4>input label</h4>
+                  <Input
+                    placeholder="eg. random text"
+                    value={label}
+                    onInput={(e: any) => setLabel(e.target.value)}
+                  />
+                </div>
+                <div className="input-form">
+                  <h4>Gas price</h4>
+                  <Input placeholder="eg. 0.0025" value={gasPrice}
+                    onInput={(e: any) => setGasPrice(e.target.value)} />
+                </div>
+                <div className="input-form">
+                  <h4>Gas denom</h4>
+                  <Input placeholder="eg. orai" value={gasDenom}
+                    onInput={(e: any) => setGasDenom(e.target.value)} />
+                </div>
+              </div>
+            </div>
+            <Form
+              schema={initSchema}
+              onChange={handleOnChange}
+              onSubmit={(data) => setInitSchemaData(data.formData)}
             />
-            <h3> Select chain name</h3>
           </div>
-          <Select
-            defaultValue={DEFAULT_CHAINMAME}
-            style={{ width: 240 }}
-            suffixIcon={<IconSelect />}
-            onSelect={(value) => {
-              setChainName(value);
-              window.chainStore.setChain(value);
-              setGasDenom(window.chainStore.current.feeCurrencies[0].coinMinimalDenom);
-            }}
-          >
-            {window.chainStore.chainInfos.map((info) => (
-              <Option key={info.chainName} value={info.chainName}>
-                {info.chainName}
-              </Option>
-            ))}
-          </Select>
-        </div>
-        <div className="wrap-form">
-          <span className="please-text">Please fill out the form below:</span>
-          {/* <div className="input-form">
-            <h4>init contract</h4>
-            <Input placeholder={`eg. {"minter": "ADDRESS"}`} value={initInput}
-              onInput={(e: any) => setInitInput(e.target.value)} />
-          </div> */}
-          <div className="input-form">
-            <h4>input label</h4>
-            <Input
-              placeholder="eg. random text"
-              value={label}
-              onInput={(e: any) => setLabel(e.target.value)}
-            />
-          </div>
-          <div className="input-form">
-            <h4>Gas price</h4>
-            <Input placeholder="eg. 0.0025" value={gasPrice}
-              onInput={(e: any) => setGasPrice(e.target.value)} />
-          </div>
-          <div className="input-form">
-            <h4>Gas denom</h4>
-            <Input placeholder="eg. orai" value={gasDenom}
-              onInput={(e: any) => setGasDenom(e.target.value)} />
-          </div>
-        </div>
-      </div>
-
+        )
+      }
       {!isLoading ? (
         (contractAddr && (
           <div className="contract-address">
@@ -198,19 +200,16 @@ const App = () => {
           <span>Deploying ...</span>
         </div>
       )}
-
-      {isBuilt && (
-        <Form
-          schema={initSchema}
-          onSubmit={(data) => handleMinter(data?.formData)}
-        // onChange={(data) => { setInitSchemaData(data) }}
-        />
-      )}
       {isDeployed && (
         <>
-          <Form schema={handleSchema} />
-          <Form schema={querySchema} onSubmit={(data) => onQuery(data?.formData)} />
+          {/* <Form schema={handleSchema} /> */}
+          <Form schema={querySchema} onSubmit={(data) => onQuery(data.formData)} noValidate />
         </>
+      )}
+      {!isBuilt && !isDeployed && !isLoading && !errorMessage && (
+        <div className="intro">
+          Start the Wasm smart contract development journey with CosmWasm IDE by building your first contract!
+        </div>
       )}
     </div>
   );
