ba701d16c47bf9d03c6282204167a706e1a4dfcd
ducphamle2 <ducphamle212@gmail.com>
2021-12-07 15:10:57 +0700

    update optimize script & csp rule

--- ext-src/optimize.sh
+++ ext-src/optimize.sh
@@ -12,8 +12,10 @@ contractdir=$(realpath "$1")
 
 basedir=$(pwd)
 build_release="${3:-true}"
+# name is extract from Cargo.toml
 name=$(basename "$contractdir")
-name=${name//-/_}
+build_name=$(pcregrep -io1 'name\s*=\s*"(.*)"' $contractdir/Cargo.toml)
+build_name=${build_name//-/_}
 cd "$contractdir"
 CARGO=$([[ -f 'Xargo.toml' && $(rustup default) =~ ^nightly.* ]] && echo 'xargo' || echo 'cargo')
 
@@ -27,10 +29,20 @@ if [ "$build_release" == 'true' ]; then
     RUSTFLAGS='-C link-arg=-s' $CARGO build -q --release --target-dir "$basedir/target" --target wasm32-unknown-unknown
     # wasm-optimize on all results
     echo "Optimizing $name.wasm"
-    wasm-opt -Os "$basedir/target/wasm32-unknown-unknown/release/$name.wasm" -o "artifacts/$name.wasm"
+    if [ ! `which wasm-opt` ] 
+    then 
+        echo "install binaryen"
+        if [ $(uname) == 'Linux' ]
+        then 
+            sudo apt install binaryen -y
+        else 
+            brew install binaryen
+        fi 
+    fi 
+    wasm-opt -Os "$basedir/target/wasm32-unknown-unknown/release/$build_name.wasm" -o "artifacts/$name.wasm"
 else
     $CARGO build -q --target-dir "$basedir/target" --target wasm32-unknown-unknown
-    cp "$basedir/target/wasm32-unknown-unknown/debug/$name.wasm" artifacts
+    cp "$basedir/target/wasm32-unknown-unknown/debug/$build_name.wasm" artifacts
 fi
 
 build_schema="${2:-true}"
--- ext-src/webview-provider.ts
+++ ext-src/webview-provider.ts
@@ -79,8 +79,9 @@ export class CosmWasmViewProvider implements vscode.WebviewViewProvider {
       const port = envText.toString().match(/(?<=[^_]PORT=)\d+/)?.[0];
       base += `http://localhost:${port}/" />`;
     } else {
+      // add connect-src in the list
       base += `${this._buildPath.with({ scheme: "vscode-resource" })}/">
-<meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src * data: blob: 'unsafe-inline'; style-src * data: blob: 'unsafe-inline'; img-src 'self' data:; font-src * data: blob: 'unsafe-inline'; script-src 'nonce-${nonce}';">`;
+<meta http-equiv="Content-Security-Policy" content="default-src 'none'; connect-src * data: blob: 'unsafe-inline'; style-src * data: blob: 'unsafe-inline'; img-src * data:; font-src * data: blob: 'unsafe-inline'; script-src 'nonce-${nonce}';">`;
     }
 
     return base;
